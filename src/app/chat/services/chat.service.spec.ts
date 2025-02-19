03087f4721cf059e08d4a208135994b1a2897f28
selidrissi <s.elidrissi1992@gmail.com>
2025-02-19 16:46:19 +0100

    Improve test

--- src/app/chat/services/chat.service.spec.ts
+++ src/app/chat/services/chat.service.spec.ts
@@ -21,4 +21,13 @@ describe('ChatService', () => {
     response.subscribe(res => expect(res).toEqual("Hello! How can I assist you today?"));
   });
 
+  it('should throw error if no response availabe', () => {
+    const userMessage = 'lorem'
+    const response = service.getUserMessageResponse(userMessage);
+    response.subscribe({
+      next: () => {},
+      error: (error) => expect(error.message).toEqual("Excuse me, I didn’t catch that. Could you say it in a different way?")
+    });
+  });
+
 });
